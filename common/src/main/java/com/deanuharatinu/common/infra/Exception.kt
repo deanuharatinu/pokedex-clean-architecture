7e4f4f3999e39d392fac774837f91def296cf88b
deanuharatinu <deanu.alt@gmail.com>
2023-12-23 19:42:18 +0700

    add `Exception`

---
+++ common/src/main/java/com/deanuharatinu/common/infra/Exception.kt
@@ -0,0 +1,7 @@
+package com.deanuharatinu.common.infra
+
+class InvalidDataException : Throwable()
+class ConnectivityException : Throwable()
+class NotFoundException : Throwable()
+class InternalServerException : Throwable()
+class UnexpectedException : Throwable()
