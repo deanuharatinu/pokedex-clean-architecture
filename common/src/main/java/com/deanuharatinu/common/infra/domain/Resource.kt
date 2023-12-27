ff6a53a467ca66f190f529aeb14962b8d882e620
deanuharatinu <deanu.alt@gmail.com>
2023-12-28 00:51:23 +0700

    add `Resource`

---
+++ common/src/main/java/com/deanuharatinu/common/infra/domain/Resource.kt
@@ -0,0 +1,6 @@
+package com.deanuharatinu.common.infra.domain
+
+sealed class Resource<T> {
+    data class Success<T>(val data: T) : Resource<T>()
+    data class Error<T>(val error: Throwable) : Resource<T>()
+}
