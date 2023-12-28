c857f31fd35f442986eb96d9626e73bb9c6dbab4
deanuharatinu <deanu.alt@gmail.com>
2023-12-28 18:25:17 +0700

    add `SaveDataException`

--- common/src/main/java/com/deanuharatinu/common/infra/Exception.kt
+++ common/src/main/java/com/deanuharatinu/common/infra/Exception.kt
@@ -1,6 +1,7 @@
 package com.deanuharatinu.common.infra
 
 class InvalidDataException : Throwable()
+class SaveDataException : Throwable()
 class ConnectivityException : Throwable()
 class NotFoundException : Throwable()
 class InternalServerException : Throwable()
